5a120165767666cf0351cc9d1c90deaf9bec70c6
luis <luisz2310@hotail.com>
2022-07-07 19:09:07 -0500

    descomentar lectura

--- Clase2.java
+++ Clase2.java
@@ -9,14 +9,12 @@ import java.util.Scanner;
 public class Clase2
 {
    public static void main(String arg[]){
-      /* Scanner input = new Scanner(System.in);
-       System.out.println("Ingrese un numero");
-       int valorEntrada = input.nextInt();
-       System.out.println("El numero es: "+valorEntrada);*/
-       
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int valorEntrada = input.nextInt();
+        System.out.println("El numero es: "+valorEntrada);
         int c = 1+1;
         System.out.println(5 + ( (c + 1 + 1) / 10 )  *  5);
         System.out.println(5 + c + 1 + 1 / 10  *  5);
-       
    }
 }
