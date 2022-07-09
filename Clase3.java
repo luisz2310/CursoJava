e1e779cec437230230dbbc44e422d535bdd006c8
JesHdez <jessicaher89@hotmail.com>
2022-07-08 21:02:17 -0500

    comparacion de enteros

--- Clase3.java
+++ Clase3.java
@@ -27,6 +27,30 @@ public class Clase3
    }
    
    public void jess(){
-       
-   }
-}
+       int n1, n2, suma;
+       System.out.printf("Ingresa primer numero: ");
+       Scanner sc1 = new Scanner(System.in);
+       n1 = sc1.nextInt();
+       System.out.printf("Ingresa segundo numero: ");
+       Scanner sc2 = new Scanner(System.in);
+       n2 = sc2.nextInt();
+       if(n1==n2){
+       System.out.printf("%d == %d%n",n1, n2 );
+        }
+       if(n1!=n2){
+       System.out.printf("%d != %d%n",n1, n2 );
+        }
+        if(n1>n2){
+       System.out.printf("%d > %d%n",n1, n2 );
+        }
+        if(n1<n2){
+       System.out.printf("%d > %d%n",n1, n2 );
+        }
+        if(n1<=n2){
+       System.out.printf("%d <= %d%n",n1, n2 );
+        }
+        if(n1>=n2){
+       System.out.printf("%d >= %d%n",n1, n2 );
+        } //se cierra el if
+   }//se cierra jess
+}//se cierra el main
