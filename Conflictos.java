645f388dd2b79438a4b81950b8857978f578c757
luis <luisz2310@hotail.com>
2022-07-11 20:07:14 -0500

    Conflicto int

---
+++ Conflictos.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Conflictos here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Conflictos
+{
+    public static void main(String args[]){
+        
+    }
+}
