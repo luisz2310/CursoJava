ea691416e4376f6b2351145dced8d2791bd646c4
luis <luisz2310@hotail.com>
2022-07-11 19:14:53 -0500

    Add beto excercise page 281

---
+++ Clase4Beto.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class Clas4Beto here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Clase4Beto
+{
+    public static void main(String args[]){
+        int c = 0;
+        int thisIsAVariable = 0;
+        int q76354 = 0;
+        int number  = 0;
+        
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+        System.out.println("This is a Java program");
+        System.out.printf("%s%n%s","This is a Java program","This is a Java program");
+        if(number == 7){
+            System.out.println("The variable number is not equal to 7");
+        }
+    }
+}
