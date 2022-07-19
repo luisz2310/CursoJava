a61a893f177a40bf5207a5b19b614dc06bf343f4
samanthamojica <samanthamojica@hotmail.com>
2022-07-18 19:09:02 -0500

    Se eliminaron los espacios en blanco

--- Ejercicio7v1.java
+++ Ejercicio7v1.java
@@ -12,7 +12,6 @@ import java.util.Scanner;
 public class Ejercicio7v1
 {
     public static void main(String args[]){
-
     Scanner sc= new Scanner(System.in);
     System.out.println("Ingrese primer numero:");  
     int num1= sc.nextInt();
