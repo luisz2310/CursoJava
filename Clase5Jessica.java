325ff4788f6d5c6355e9a5d0ad3d0aeb200a844d
JesHdez <jessicaher89@hotmail.com>
2022-07-19 18:56:58 -0500

    se corrigio el error

--- Clase5Jessica.java
+++ Clase5Jessica.java
@@ -14,19 +14,13 @@ public class Clase5Jessica
         //Lea el primer entero del usuario y guárdelo en el int variable x.
         //Cree un Escáner llamado input que lea valores del entrada estándar.
         System.out.println("Introduzca el primer numero:");
-        Scanner num1 = new Scanner (System.in);
-        int x = num1.nextInt();
-        
+        Scanner s = new Scanner (System.in);
+        int x = s.nextInt();
         System.out.println("Introduzca el segundo numero:");
-        Scanner num2 = new Scanner (System.in);
-        int y = num2.nextInt();
-        
+        int y = s.nextInt();
         System.out.println("Introduzca el tercer numero:");
-        Scanner num3 = new Scanner (System.in);
-        int z = num3.nextInt();
-    
+        int z = s.nextInt();
         result= x*y*z;
-        
         System.out.printf("El producto de los numeros es:%d%n", result);
     }
 }
