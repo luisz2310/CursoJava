c98b9f5adccee3602cf36c890c5828b1c5ac8fb5
JesHdez <jessicaher89@hotmail.com>
2022-07-20 00:22:41 -0500

    ejercicio 8 terminado

---
+++ Ejercicio8Jessica.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Ejercicio8 here.
+ * 
+ * @author (jessica) 
+ * @version (19/julio/2022)
+ */
+public class Ejercicio8Jessica
+{
+    public static void main (String args[]){
+        System.out.println("*********      ***       *         *");
+        System.out.println("*       *    *     *    ***       * *");
+        System.out.println("*       *   *       *  *****     *   *");
+        System.out.println("*       *   *       *    *      *     *");
+        System.out.println("*       *   *       *    *     *       *");
+        System.out.println("*       *   *       *    *      *     *");
+        System.out.println("*       *   *       *    *       *   *");
+        System.out.println("*       *    *     *     *        * *");
+        System.out.print("*********      ***       *         *");
+    }
+}
